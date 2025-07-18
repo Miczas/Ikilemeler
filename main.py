ce5ec95a651bb2325d60a82e81e2ec9df67264f9
Michal Mikolajczyk <micha.mikolajczyk@gmail.com>
2025-07-18 08:26:08 +0200

    Refactored the code
    Cleaned the code.
    Added the dictionary from https://github.com/mertemin/turkish-word-list/blob/master/words.txt

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
+def find_double_words(input_file: str, output_file: str) -> None:
+    """
+    Reads the input_file line by line and writes lines containing
+    Turkish double words (ikileme) to the output_file.
+    """
+    with open(input_file, 'r', encoding='utf-8') as infile, \
+         open(output_file, 'w', encoding='utf-8') as outfile:
+        for line in infile:
+            words = line.strip().split()
+            if words and words.count(words[0]) > 1:
+                outfile.write(line)
 
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    file2 = open("FirstIkilemeDictionary.txt", "w", encoding='utf-8')
-    file1 = open('words.txt', 'r', encoding='utf-8')
-    Lines = file1.readlines()
-
-    count = 0
-    # Strips the newline character
-    for line in Lines:
-        words = line.split()
-        if (words.count(words[0])>1):
-            file2.write(line)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    find_double_words('words.txt', 'FirstIkilemeDictionary.txt')
